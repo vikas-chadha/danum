203e1f55e75e81a1dba51936804f664b3e4bddc9
pythongithub-softuvo <python.github@softuvo.in>
2024-07-16 14:48:55 +0530

    add static files

--- danum_backend/settings.py
+++ danum_backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7nag63xvhr#d8ifj*5t)z0v%q@3cb&dmn6qampkcukeo$yimsn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
